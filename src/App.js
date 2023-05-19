3397b9b269fe001082780d730dad46a1b9a741a0
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-05-18 19:40:00 -0700

    added line graph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 import './App.css'
 import AllCurrency from './AllCurrency'
 import Header from './Header'
--- src/CurrencyPair.js
+++ src/CurrencyPair.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import Chart from 'chart.js/auto'
 
 function CurrencyPair() {
   const [rates, setRates] = useState({})
@@ -6,20 +7,74 @@ function CurrencyPair() {
   const [toCurrency, setToCurrency] = useState('EUR')
   const [amount, setAmount] = useState(1)
   const [result, setResult] = useState(0)
+  const [chartInstance, setChartInstance] = useState(null) // Store the chart instance
 
   useEffect(() => {
     fetch(`https://api.frankfurter.app/latest?from=${fromCurrency}`)
       .then(response => response.json())
       .then(data => setRates(data.rates))
-  }, [fromCurrency])
+    getHistoricDates()
+  }, [fromCurrency, toCurrency])
 
-  const handleFromCurrencyChange = e => setFromCurrency(e.target.value)
-  const handleToCurrencyChange = e => setToCurrency(e.target.value)
-  const handleAmountChange = e => setAmount(e.target.value)
+  const handleFromCurrencyChange = e => {
+    setFromCurrency(e.target.value)
+  }
 
+  const handleToCurrencyChange = e => {
+    setToCurrency(e.target.value)
+  }
+
+  const handleAmountChange = e => {
+    setAmount(e.target.value)
+  }
   const handleConversion = () => {
     const rate = rates[toCurrency]
     setResult(amount * rate)
+  }
+
+  const chartRef = useRef(null)
+
+  const getHistoricDates = async () => {
+    const endDate = new Date().toISOString().split('T')[0]
+    const startDate = new Date(
+      new Date().getTime() - 30 * 24 * 6660 * 60 * 1000
+    )
+      .toISOString()
+      .split('T')[0]
+
+    try {
+      const response = await fetch(
+        `https://api.frankfurter.app/${startDate}..${endDate}?from=${fromCurrency}&to=${toCurrency}`
+      )
+      const data = await response.json()
+      const chartLabels = Object.keys(data.rates)
+      const chartData = Object.values(data.rates).map(rate => rate[toCurrency])
+      const chartLabel = `${fromCurrency}/${toCurrency}`
+
+      const chartConfig = {
+        type: 'line',
+        data: {
+          labels: chartLabels,
+          datasets: [
+            {
+              label: chartLabel,
+              data: chartData,
+              fill: false,
+              tension: 0,
+            },
+          ],
+        },
+      }
+
+      if (chartInstance) {
+        chartInstance.destroy() // Destroy the previous chart instance
+      }
+
+      const newChart = new Chart(chartRef.current, chartConfig)
+      setChartInstance(newChart)
+    } catch (err) {
+      console.log('ERR', err)
+    }
   }
 
   return (
@@ -50,6 +105,7 @@ function CurrencyPair() {
         </button>
       </div>
       <h2>Result: {result}</h2>
+      <canvas ref={chartRef} />
     </div>
   )
 }
